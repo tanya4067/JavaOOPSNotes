ad31f64dc72fd018209e5a6b5fc24412d40c9bea
Resume <59206519+tanya4067@users.noreply.github.com>
2025-06-21 00:52:17 +0530

    Create finalKeyword.java

---
+++ finalKeyword.java
@@ -0,0 +1,4 @@
+//final keyword is used in three places - variable , method, classes
+// variable - constant (the value cannot be changed )
+// class - The class cannot be inheritated
+// method - the method cannot be inheritated.
